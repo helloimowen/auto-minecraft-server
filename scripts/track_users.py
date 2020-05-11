c00842ff0c7d04ca03119f0b1648e5932b6fcb65
Owen Sanders <owen@Owens-MacBook-Air.local>
2020-05-11 19:53:07 -0400

    started on track user script

---
+++ scripts/track_users.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime, timedelta
+
+current_time = now.strftime("%Y-%m-%d %H:%M:%S") - timedelta(days = 1)
+
+# 2020-05-11 01:00:00
+
+
+# def write_time_file():
+#     f = open("last_run.txt", "w")
+#     f.write(current_time)
+#     f.close()
+
+
+
+
+
+stream = os.popen('journalctl --output=cat --since="' + current_time + '" -a -u  minecraft')
+output = stream.read()
+
+print(output)
+
+'''
+statuses I care about: 
+
+ lost connection
+ logged in with entity id 
+
+ '''
